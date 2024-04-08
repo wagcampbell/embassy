65ec977b4bb1c3f8b3c5acf789fe9c7b9227e4be
wagcampbell <49646556+wagcampbell@users.noreply.github.com>
2024-04-08 19:15:53 -0400

    Support non-secure flash operations

--- embassy-stm32/src/flash/u5.rs
+++ embassy-stm32/src/flash/u5.rs
@@ -13,27 +13,52 @@ pub(crate) const fn get_flash_regions() -> &'static [&'static FlashRegion] {
     &FLASH_REGIONS
 }
 
+fn is_trustzone_enabled() -> bool {
+    pac::FLASH.optr().read().tzen()
+}
+
 pub(crate) unsafe fn lock() {
-    pac::FLASH.seccr().modify(|w| w.set_lock(true));
+    if is_trustzone_enabled() {
+        pac::FLASH.seccr().modify(|w| w.set_lock(true));
+    } else {
+        pac::FLASH.nscr().modify(|w| w.set_lock(true));
+    }
 }
 
 pub(crate) unsafe fn unlock() {
-    if pac::FLASH.seccr().read().lock() {
-        pac::FLASH.seckeyr().write_value(0x4567_0123);
-        pac::FLASH.seckeyr().write_value(0xCDEF_89AB);
+    if is_trustzone_enabled() {
+        if pac::FLASH.seccr().read().lock() {
+            pac::FLASH.seckeyr().write_value(0x4567_0123);
+            pac::FLASH.seckeyr().write_value(0xCDEF_89AB);
+        }
+    } else {
+        if pac::FLASH.nscr().read().lock() {
+            pac::FLASH.nskeyr().write_value(0x4567_0123);
+            pac::FLASH.nskeyr().write_value(0xCDEF_89AB);
+        }
     }
 }
 
 pub(crate) unsafe fn enable_blocking_write() {
     assert_eq!(0, WRITE_SIZE % 4);
 
-    pac::FLASH.seccr().write(|w| {
-        w.set_pg(pac::flash::vals::SeccrPg::B_0X1);
-    });
+    if is_trustzone_enabled() {
+        pac::FLASH.seccr().write(|w| {
+            w.set_pg(pac::flash::vals::SeccrPg::B_0X1);
+        });
+    } else {
+        pac::FLASH.nscr().write(|w| {
+            w.set_pg(pac::flash::vals::NscrPg::B_0X1);
+        });
+    }
 }
 
 pub(crate) unsafe fn disable_blocking_write() {
-    pac::FLASH.seccr().write(|w| w.set_pg(pac::flash::vals::SeccrPg::B_0X0));
+    if is_trustzone_enabled() {
+        pac::FLASH.seccr().write(|w| w.set_pg(pac::flash::vals::SeccrPg::B_0X0));
+    } else {
+        pac::FLASH.nscr().write(|w| w.set_pg(pac::flash::vals::NscrPg::B_0X0));
+    }
 }
 
 pub(crate) unsafe fn blocking_write(start_address: u32, buf: &[u8; WRITE_SIZE]) -> Result<(), Error> {
@@ -50,19 +75,39 @@ pub(crate) unsafe fn blocking_write(start_address: u32, buf: &[u8; WRITE_SIZE]) 
 }
 
 pub(crate) unsafe fn blocking_erase_sector(sector: &FlashSector) -> Result<(), Error> {
-    pac::FLASH.seccr().modify(|w| {
-        w.set_per(pac::flash::vals::SeccrPer::B_0X1);
-        w.set_pnb(sector.index_in_bank)
-    });
+    if is_trustzone_enabled() {
+        pac::FLASH.seccr().modify(|w| {
+            w.set_per(pac::flash::vals::SeccrPer::B_0X1);
+            w.set_pnb(sector.index_in_bank)
+        });
+        
+    } else {
+        pac::FLASH.nscr().modify(|w| {
+            w.set_per(pac::flash::vals::NscrPer::B_0X1);
+            w.set_pnb(sector.index_in_bank)
+        });
+    }
 
-    pac::FLASH.seccr().modify(|w| {
-        w.set_strt(true);
-    });
-
+    if is_trustzone_enabled() {
+        pac::FLASH.seccr().modify(|w| {
+            w.set_strt(true);
+        });
+    } else {
+        pac::FLASH.nscr().modify(|w| {
+            w.set_strt(true);
+        });
+    }
+    
     let ret: Result<(), Error> = blocking_wait_ready();
-    pac::FLASH
+    if is_trustzone_enabled() {
+        pac::FLASH
         .seccr()
         .modify(|w| w.set_per(pac::flash::vals::SeccrPer::B_0X0));
+    } else {
+        pac::FLASH
+        .nscr()
+        .modify(|w| w.set_per(pac::flash::vals::NscrPer::B_0X0));
+    }
     clear_all_err();
     ret
 }
@@ -70,35 +115,67 @@ pub(crate) unsafe fn blocking_erase_sector(sector: &FlashSector) -> Result<(), E
 pub(crate) unsafe fn clear_all_err() {
     // read and write back the same value.
     // This clears all "write 1 to clear" bits.
-    pac::FLASH.secsr().modify(|_| {});
+    if is_trustzone_enabled() {
+        pac::FLASH.secsr().modify(|_| {});
+    } else {
+        pac::FLASH.nssr().modify(|_| {});
+    }
 }
 
 unsafe fn blocking_wait_ready() -> Result<(), Error> {
     loop {
-        let sr = pac::FLASH.secsr().read();
+        if is_trustzone_enabled() {
+            let sr = pac::FLASH.secsr().read();
 
-        if !sr.bsy() {
-            if sr.pgserr() {
-                return Err(Error::Seq);
+            if !sr.bsy() {
+                if sr.pgserr() {
+                    return Err(Error::Seq);
+                }
+    
+                if sr.sizerr() {
+                    return Err(Error::Size);
+                }
+    
+                if sr.pgaerr() {
+                    return Err(Error::Unaligned);
+                }
+    
+                if sr.wrperr() {
+                    return Err(Error::Protected);
+                }
+    
+                if sr.progerr() {
+                    return Err(Error::Prog);
+                }
+    
+                return Ok(());
             }
+        } else {
+            let sr = pac::FLASH.nssr().read();
 
-            if sr.sizerr() {
-                return Err(Error::Size);
+            if !sr.bsy() {
+                if sr.pgserr() {
+                    return Err(Error::Seq);
+                }
+    
+                if sr.sizerr() {
+                    return Err(Error::Size);
+                }
+    
+                if sr.pgaerr() {
+                    return Err(Error::Unaligned);
+                }
+    
+                if sr.wrperr() {
+                    return Err(Error::Protected);
+                }
+    
+                if sr.progerr() {
+                    return Err(Error::Prog);
+                }
+    
+                return Ok(());
             }
-
-            if sr.pgaerr() {
-                return Err(Error::Unaligned);
-            }
-
-            if sr.wrperr() {
-                return Err(Error::Protected);
-            }
-
-            if sr.progerr() {
-                return Err(Error::Prog);
-            }
-
-            return Ok(());
         }
     }
 }
